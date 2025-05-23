bdb774859fede17c34ccdbd48392be3f0da409b9
Nidhin Rajan <nidhinrajan91@gmail.com>
2025-05-23 15:09:17 +0530

    Dynamic App 9

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-let dialogues = [];
-
 async function fetchDialogues() {
   const response = await fetch("http://localhost:3000/dialogue");
   const d = await response.json();
 
-  // No need to randomize since backend sends one
   document.getElementById("dialogue").innerText = `"${d.dialogue}"`;
   document.getElementById("details").innerText = `— ${d.actor}, ${d.movie} (Shown ${d.shown_count}x)`;
 }
