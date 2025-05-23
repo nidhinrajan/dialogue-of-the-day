6a3712168db189333b038903e2501364bf6055eb
Nidhin Rajan <nidhinrajan91@gmail.com>
2025-05-23 13:11:09 +0530

    Dynamic app 6

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ async function fetchDialogues() {
       apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsYnNseGxycWtvaWplaHVocXV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5ODIxMTUsImV4cCI6MjA2MzU1ODExNX0.SWXjJp_qfhQ08uPcTsGrzwNPkP6i5umyqvRxd86iE2A",
       Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsYnNseGxycWtvaWplaHVocXV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5ODIxMTUsImV4cCI6MjA2MzU1ODExNX0.SWXjJp_qfhQ08uPcTsGrzwNPkP6i5umyqvRxd86iE2A"
     }
+    cache: "no-store"
   });
 
   dialogues = await response.json();
